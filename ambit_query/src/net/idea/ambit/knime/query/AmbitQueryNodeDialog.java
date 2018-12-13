8ede0b51f8a011d4d50faeb546b7b7885a3ea916
ntk73 <nick@uni-plovdiv.net>
2018-12-13 22:59:31 +0200

    Code ident reformating

--- ambit_query/src/net/idea/ambit/knime/query/AmbitQueryNodeDialog.java
+++ ambit_query/src/net/idea/ambit/knime/query/AmbitQueryNodeDialog.java
@@ -17,21 +17,21 @@ import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
  */
 public class AmbitQueryNodeDialog extends DefaultNodeSettingsPane {
 
-    /**
-     * New pane for configuring AmbitQuery node dialog.
-     * This is just a suggestion to demonstrate possible default dialog
-     * components.
-     */
-    protected AmbitQueryNodeDialog() {
-        super();
-        
-        addDialogComponent(new DialogComponentNumber(
-                new SettingsModelIntegerBounded(
-                    AmbitQueryNodeModel.CFGKEY_COUNT,
-                    AmbitQueryNodeModel.DEFAULT_COUNT,
-                    Integer.MIN_VALUE, Integer.MAX_VALUE),
-                    "Counter:", /*step*/ 1, /*componentwidth*/ 5));
-                    
-    }
+	/**
+	 * New pane for configuring AmbitQuery node dialog.
+	 * This is just a suggestion to demonstrate possible default dialog
+	 * components.
+	 */
+	protected AmbitQueryNodeDialog() {
+		super();
+
+		addDialogComponent(new DialogComponentNumber(
+				new SettingsModelIntegerBounded(
+						AmbitQueryNodeModel.CFGKEY_COUNT,
+						AmbitQueryNodeModel.DEFAULT_COUNT,
+						Integer.MIN_VALUE, Integer.MAX_VALUE),
+				"Counter:", /*step*/ 1, /*componentwidth*/ 5));
+
+	}
 }
 
--- ambit_query/src/net/idea/ambit/knime/query/AmbitQueryNodePlugin.java
+++ ambit_query/src/net/idea/ambit/knime/query/AmbitQueryNodePlugin.java
@@ -17,49 +17,49 @@ import org.osgi.framework.BundleContext;
  * @author Ideaconsult Ltd.
  */
 public class AmbitQueryNodePlugin extends Plugin {
-    // The shared instance.
-    private static AmbitQueryNodePlugin plugin;
+	// The shared instance.
+	private static AmbitQueryNodePlugin plugin;
 
-    /**
-     * The constructor.
-     */
-    public AmbitQueryNodePlugin() {
-        super();
-        plugin = this;
-    }
+	/**
+	 * The constructor.
+	 */
+	public AmbitQueryNodePlugin() {
+		super();
+		plugin = this;
+	}
 
-    /**
-     * This method is called upon plug-in activation.
-     * 
-     * @param context The OSGI bundle context
-     * @throws Exception If this plugin could not be started
-     */
-    @Override
-    public void start(final BundleContext context) throws Exception {
-        super.start(context);
+	/**
+	 * This method is called upon plug-in activation.
+	 * 
+	 * @param context The OSGI bundle context
+	 * @throws Exception If this plugin could not be started
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
 
-    }
+	}
 
-    /**
-     * This method is called when the plug-in is stopped.
-     * 
-     * @param context The OSGI bundle context
-     * @throws Exception If this plugin could not be stopped
-     */
-    @Override
-    public void stop(final BundleContext context) throws Exception {
-        super.stop(context);
-        plugin = null;
-    }
+	/**
+	 * This method is called when the plug-in is stopped.
+	 * 
+	 * @param context The OSGI bundle context
+	 * @throws Exception If this plugin could not be stopped
+	 */
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
 
-    /**
-     * Returns the shared instance.
-     * 
-     * @return Singleton instance of the Plugin
-     */
-    public static AmbitQueryNodePlugin getDefault() {
-        return plugin;
-    }
+	/**
+	 * Returns the shared instance.
+	 * 
+	 * @return Singleton instance of the Plugin
+	 */
+	public static AmbitQueryNodePlugin getDefault() {
+		return plugin;
+	}
 
 }
 
